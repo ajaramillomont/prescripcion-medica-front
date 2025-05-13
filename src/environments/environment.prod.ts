b074823f326b995ca583d041d200119ca6111db6
ajaramillomont <ajaramillomont@gmail.com>
2025-05-12 22:09:28 -0500

    environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
-//configuramos la url de la API
 export const environment = {
   production: true,
   apiUrl: 'https://prescripcion-medica-backend-production.up.railway.app'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
-//configuramos la url de la API
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000'
